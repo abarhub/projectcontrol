a484769e3761887ff6860f19428d2cfbb40b0b72
abarhub <abarhub@gmail.com>
2025-08-10 21:49:48 +0200

    début de structuration du projet

---
+++ server/src/main/java/org/projectix/server/ProjectixApplication.java
@@ -0,0 +1,15 @@
+package org.projectix.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectixApplication.class, args);
+    }
+
+}
+
+
