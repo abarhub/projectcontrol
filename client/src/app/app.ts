2b0de72e6d55bd8cda748edf377105f448d260ab
abarhub <abarhub@gmail.com>
2025-08-15 09:20:00 +0200

    ajout de bootstrap et fontawesome-free

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TableauPrincipal} from './tableau-principal/tableau-principal';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TableauPrincipal],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
