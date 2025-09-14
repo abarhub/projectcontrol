64884eb83ded5a4b325091754d76b90b46814be4
abarhub <abarhub@gmail.com>
2025-09-14 11:16:55 +0200

    nettoyage de code

--- server/src/main/java/org/projectcontrol/server/controler/RechercheControler.java
+++ server/src/main/java/org/projectcontrol/server/controler/RechercheControler.java
@@ -1,7 +1,5 @@
 package org.projectcontrol.server.controler;
 
-import org.projectcontrol.server.dto.LigneResultatDto;
-import org.projectcontrol.server.dto.ProjetDto;
 import org.projectcontrol.server.dto.ReponseRechercheInitialDto;
 import org.projectcontrol.server.dto.ReponseRechercheSuivanteDto;
 import org.projectcontrol.server.service.RechercheService;
@@ -10,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
